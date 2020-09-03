d81b9b72878b32efd2890d70abc6e03732e3acdb
Yuri Duque <yurithielmann83@gmail.com>
2020-09-02 23:43:16 -0300

    criando verificaçao de vencedor

--- jogo-da-velha/src/TicTacToe.js
+++ jogo-da-velha/src/TicTacToe.js
@@ -1,18 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './TicTacToe.css';
 
 function TicTacToe() {
   const emptyBoard = Array(9).fill("");
   const [board, setBoard] = useState(emptyBoard);
-
   const [currentPlayer, setCurrentPlayer] = useState("O");
 
   const handleCellClick = (index) => {
-    const newBoard = board.map((item, itemIndex) => itemIndex === index ? currentPlayer : item );
+    if (board[index] !== "") return null;
+
+    const newBoard = board.map((item, itemIndex) => itemIndex === index ? currentPlayer : item);
     setBoard(newBoard);
 
-    setCurrentPlayer(currentPlayer === "X" ? "O" : "X" )
+    // trocando usuário atual
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
   }
+
+  const checkWinner = () => {
+    const possibleWatsToWin = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]],
+    ];
+
+    possibleWatsToWin.forEach(cells => {
+      if(cells.every(cell => cell === "X")) console.log("X venceu");
+
+      else if(cells.every(cell => cell === "O")) console.log("O venceu");
+    })
+  }
+
+  useEffect(checkWinner, [board]);
 
   return (
     <main>
